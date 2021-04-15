373adb121a14bebfab19534bcc7200ed94376e04
Łukasz Zaraska <lukasz.zaraska@microsoft.wsei.edu.pl>
2021-04-15 16:38:28 +0200

    Add project files.

---
+++ Architektura_systemu/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Architektura_systemu
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
